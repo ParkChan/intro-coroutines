cd7b2360c10fea31f2db1fec1af0eb7636c42ce6
ParkChan <hydrarkk@naver.com>
2021-09-23 01:43:39 +0900

    [수정/테스트코드] Step5 Concurrency : Request5NotCancellable
    예제코드 가이드 참조로 실습 코드 추가로 수정

--- src/tasks/Request4Suspend.kt
+++ src/tasks/Request4Suspend.kt
@@ -3,13 +3,14 @@ package tasks
 import contributors.*
 
 suspend fun loadContributorsSuspend(service: GitHubService, req: RequestData): List<User> {
-    val repos = service.getOrgRepos(req.org)
-        .also { logRepos(req, it) }
-        .body() ?: listOf()
+    val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
 
     return repos.flatMap { repo ->
         service.getRepoContributors(req.org, repo.name)
-            .also { logUsers(repo, it) }
-            .bodyList()
+                .also { logUsers(repo, it) }
+                .bodyList()
     }.aggregate()
 }
