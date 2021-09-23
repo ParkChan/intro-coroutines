b11f19f145dd57b75d31de4a400ebe0b4267744f
ParkChan <hydrarkk@naver.com>
2021-09-24 00:19:27 +0900

    [수정/테스트코드] Step8 Channels

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -6,11 +6,30 @@ import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 suspend fun loadContributorsChannels(
-    service: GitHubService,
-    req: RequestData,
-    updateResults: suspend (List<User>, completed: Boolean) -> Unit
+        service: GitHubService,
+        req: RequestData,
+        updateResults: suspend (List<User>, completed: Boolean) -> Unit,
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+                .getOrgRepos(req.org)
+                .also { logRepos(req, it) }
+                .bodyList()
+
+        val channel = Channel<List<User>>()
+        for (repo in repos) {
+            launch {
+                val users = service.getRepoContributors(req.org, repo.name)
+                        .also { logUsers(repo, it) }
+                        .bodyList()
+                channel.send(users)
+            }
+        }
+        var allUsers = emptyList<User>()
+        repeat(repos.size) {
+            val users = channel.receive()
+            allUsers = (allUsers + users).aggregate()
+            updateResults(allUsers, it == repos.lastIndex)
+        }
     }
 }
